af758f966027ce634efbc4ca09631255ae2657e8
Shahar Tefler <shahartefler@gmail.com>
2020-05-17 22:33:44 +0300

    Expanded python script - added login and signup request + support in json packets

--- clientTrivia.py
+++ clientTrivia.py
@@ -1,10 +1,13 @@
 import socket
+import json
 
 SERVER_IP = "127.0.0.1"
 SERVER_PORT = 1050
 MAX_LEN = 1024
 HELLO_MSG = "Hello"
 
+# Codes for different types of requests
+Codes = {"LOGIN_CODE": "1", "SIGN_UP_CODE": "2"}
 
 def make_socket():
     """
@@ -40,12 +43,79 @@ def send_information(sock, msg):
     sock.sendall(msg.encode())
 
 
+def send_sign_up_request(sock):
+    """
+    Inputs the parameters for the sign-up request, and sends the request to the server.
+    :param sock: The socket connecting the server and the client
+    :return: None
+    """
+
+    # Getting sign up information from the user
+    print("Enter username: ")
+    username = input()
+    print("Enter password: ")
+    password = input()
+    print("Enter email: ")
+    email = input()
+
+    # Creating a json object from the information
+    sign_up_req = {"username": username,
+                   "password": password,
+                   "email": email}
+
+    sign_up_req = sign_up_req.dumps()  # Getting the json as a string
+    str_packet = Codes["SIGN_UP_CODE"] + str(len(sign_up_req)) + sign_up_req  # Building the packet according to the protocol
+    sock.sendall(bytes(str_packet, encoding="utf-8"))
+
+
+def send_login_request(sock):
+    """
+    Inputs the parameters for the login request, and sends the request to the server.
+    :param sock: The socket connecting the server and the client
+    :return: None
+    """
+
+    # Getting sign up information from the user
+    print("Enter username: ")
+    username = input()
+    print("Enter password: ")
+    password = input()
+
+    # Creating a json object from the information
+    login_req = {"username": username,
+                 "password": password }
+
+    login_req = login_req.dumps()  # Getting the json as a string
+    str_packet = Codes["LOGIN_CODE"] + str(len(login_req)) + login_req  # Building the packet according to the protocol
+    sock.sendall(bytes(str_packet, encoding="utf-8"))
+
+
+def receive_response(sock):
+    """
+    Receives the response from the server, and prints it.
+    :param sock: The socket connecting the server and the client
+    :return: None.
+    """
+    packet = sock.recv(MAX_LEN)
+    j = json.load(packet.decode('utf-8'))
+    print("Received from server: " + j.dumps)
+
+
 def main():
-    sock = make_socket()
-    msg = receive_information(sock)
-    print(msg)
-    if msg == HELLO_MSG:
-        send_information(sock, HELLO_MSG)
+    sock = make_socket()                         # Creating a listening socket
+    msg = receive_information(sock)  # Getting the first "Hello" message from the server
+    print("Message from server: " + msg)
+
+    if msg == HELLO_MSG:                               # Checking message's content
+        send_information(listening_socket, HELLO_MSG)  # Responding to the server
+
+    send_sign_up_request(sock)  # Sending a Sign up request
+    receive_response(sock)      # Getting server's response
+
+    send_login_request(sock)    # Sending a Log in request
+    receive_response(sock)      # Getting server's response
+
+    sock.close()
 
 
 if __name__ == "__main__":
