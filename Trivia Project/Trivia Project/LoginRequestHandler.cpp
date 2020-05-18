71d69495c63455cf7e42b9dd65b250e7cb46923c
Shahar Tefler <shahartefler@gmail.com>
2020-05-18 16:45:17 +0300

    fixed sign up request in Login request handler

--- Trivia Project/Trivia Project/LoginRequestHandler.cpp
+++ Trivia Project/Trivia Project/LoginRequestHandler.cpp
@@ -8,6 +8,15 @@ bool LoginRequestHandler::isRequestRelevant(RequestInfo info)
 RequestResult LoginRequestHandler::handleRequest(RequestInfo info)
 {
 	JsonRequestPacketDeserializer::deserializeLoginRequest(info.buffer);
-	LoginResponse response = { 1 };
-	return RequestResult{ JsonResponsePacketSerializer::serializeResponse(response) };
+
+	if (info.requestId == LOGIN_CODE)
+	{
+		LoginResponse response = { 1 };
+		return RequestResult{ JsonResponsePacketSerializer::serializeResponse(response) };
+	}
+	else if (info.requestId == SIGN_UP_CODE)
+	{
+		SignupResponse response = { 1 };
+		return RequestResult{ JsonResponsePacketSerializer::serializeResponse(response) };
+	}
 }
